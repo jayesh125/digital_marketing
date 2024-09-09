f025f002b60b6e1e3893dabcf3808ca46d4021c9
Jayesh Shinde <48760468+jayesh125@users.noreply.github.com>
2024-09-09 22:30:03 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,9 +30,9 @@ function typeText(textId) {
     if (letter.length === currentText.length) {
         count++;
         index = 0;
-        typingTimeout = setTimeout(() => typeText(textId), 2000); // Pause before starting the next word
+        typingTimeout = setTimeout(() => typeText(textId), 1500); // Pause before starting the next word
     } else {
-        typingTimeout = setTimeout(() => typeText(textId), 150); // Typing speed
+        typingTimeout = setTimeout(() => typeText(textId), 140); // Typing speed
     }
 }
 
@@ -120,12 +120,112 @@ function adjustContentMargin() {
 window.addEventListener('load', adjustContentMargin);
 window.addEventListener('resize', adjustContentMargin);
 
-const sliderTrack = document.querySelector('.slide-track');
+const carouselWrapper = document.querySelector('.carousel-wrapper');
 
-sliderTrack.addEventListener('mouseover', () => {
-  sliderTrack.style.animationPlayState = 'paused';
+// Pause animation on hover
+carouselWrapper.addEventListener('mouseover', () => {
+  carouselWrapper.style.animationPlayState = 'paused';
 });
 
-sliderTrack.addEventListener('mouseout', () => {
-  sliderTrack.style.animationPlayState = 'running';
+// Resume animation when not hovering
+carouselWrapper.addEventListener('mouseout', () => {
+  carouselWrapper.style.animationPlayState = 'running';
 });
+
+const counters = document.querySelectorAll('.counter');
+const speed = 10000; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = Math.ceil(count + inc);
+			// Call function every ms
+			setTimeout(updateCount, 20);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
+
+const items = document.querySelectorAll('.item'),
+  controls = document.querySelectorAll('.control'),
+  headerItems = document.querySelectorAll('.item-header'),
+  descriptionItems = document.querySelectorAll('.item-description'),
+  activeDelay = .76,
+  interval = 5000;
+
+let current = 0;
+
+const slider = {
+  init: () => {
+    controls.forEach(control => control.addEventListener('click', (e) => { slider.clickedControl(e) }));
+    controls[current].classList.add('active');
+    items[current].classList.add('active');
+  },
+  nextSlide: () => { // Increment current slide and add active class
+    slider.reset();
+    if (current === items.length - 1) current = -1; // Check if current slide is last in array
+    current++;
+    controls[current].classList.add('active');
+    items[current].classList.add('active');
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+  },
+  clickedControl: (e) => { // Add active class to clicked control and corresponding slide
+    slider.reset();
+    clearInterval(intervalF);
+
+    const control = e.target,
+      dataIndex = Number(control.dataset.index);
+
+    control.classList.add('active');
+    items.forEach((item, index) => { 
+      if (index === dataIndex) { // Add active class to corresponding slide
+        item.classList.add('active');
+      }
+    })
+    current = dataIndex; // Update current slide
+    slider.transitionDelay(headerItems);
+    slider.transitionDelay(descriptionItems);
+    intervalF = setInterval(slider.nextSlide, interval); // Fire that bad boi back up
+  },
+  reset: () => { // Remove active classes
+    items.forEach(item => item.classList.remove('active'));
+    controls.forEach(control => control.classList.remove('active'));
+  },
+  transitionDelay: (items) => { // Set incrementing css transition-delay for .item-header & .item-description, .vertical-part, b elements
+    let seconds;
+    
+    items.forEach(item => {
+      const children = item.childNodes; // .vertical-part(s)
+      let count = 1,
+        delay;
+      
+      item.classList.value === 'item-header' ? seconds = .015 : seconds = .007;
+
+      children.forEach(child => { // iterate through .vertical-part(s) and style b element
+        if (child.classList) {
+          item.parentNode.classList.contains('active') ? delay = count * seconds + activeDelay : delay = count * seconds;
+          child.firstElementChild.style.transitionDelay = `${delay}s`; // b element
+          count++;
+        }
+      })
+    })
+  },
+}
+
+let intervalF = setInterval(slider.nextSlide, interval);
+slider.init();
